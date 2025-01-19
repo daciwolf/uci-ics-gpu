d396da7a2ac889235c84af5e35c920c46d4a13e2
daciwolf <davidculciar@gmail.com>
2025-01-18 23:49:50 -0700

    changed load.sh to properly create output files

--- load.sh
+++ load.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+#!/bin/bash
+#SBATCH --ntasks=1             # Number of tasks to run (equal to 1 CPU/core per task)
+#SBATCH --gres=gpu:1           # Number of GPUs (per node)
+#SBATCH --mem=4000M            # Memory (per node)
+#SBATCH --time=0-03:00         # Time (DD-HH:MM)
+#SBATCH -o load.out        # File to which STDOUT will be written, %j inserts job ID
+#SBATCH -e load.err        # File to which STDERR will be written, %j inserts job ID
 
-# Exit on error
 set -e
 
 # Create a virtual environment (if not already created)
